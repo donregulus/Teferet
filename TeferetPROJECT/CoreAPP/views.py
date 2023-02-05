ccd264307758e6c73067c7d8719d4734a75804c8
DonMoringa <you@example.com>
2023-02-05 01:46:08 +0100

    Fix errors from server side

--- TeferetPROJECT/CoreAPP/views.py
+++ TeferetPROJECT/CoreAPP/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, "CoreAPP/index.html")
+    return render(request, "CoreAPP/Index.html")
 
 
 
