1b5c530066d390c589f040904d8fe17b5b902d73
DonMoringa <you@example.com>
2023-04-09 21:06:15 +0200

    TEF-13 : Create/Edit/Delete a story

--- TeferetPROJECT/BlogAPP/models.py
+++ TeferetPROJECT/BlogAPP/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
 
-# Create your models here.
+
+
+class Tags(models.Model):
+    tid         = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    title       = models.CharField(max_length=150,unique=True)
+    createdDate = models.DateTimeField(auto_now_add=True )
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class Blog(models.Model):
+    bid             = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    createdDate     = models.DateTimeField(auto_now_add=True )
+    title           = models.CharField(max_length=150)
+    image           = models.ImageField(upload_to="Blogs",default="blog.jpg")    
+    fullDescription = models.TextField(null=False,blank=False)            
+    summary         = models.TextField(null=False,blank=False)     
+    tags            = models.ManyToManyField(Tags)
+
+    def __str__(self) -> str:
+        return self.title
+    
+class Comment(models.Model):    
+    cid         = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    user        = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+    blog        = models.ForeignKey(Blog, on_delete=models.CASCADE, null=False)
+    createdDate = models.DateField(auto_now_add=True)
+    commentText = models.TextField(null=False,blank=False)                
+
+    def __str__(self):
+        return str(self.commentText)
+
+
+
+    
