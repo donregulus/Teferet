3df840e9273782a53242c9c1d04141d5e387da81
fossi <stroalgo@gmail.com>
2023-11-10 05:35:58 +0100

    TEF-44 : Enable mysql DB in production

--- TeferetPROJECT/TeferetPROJECT/settings.py
+++ TeferetPROJECT/TeferetPROJECT/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+#Load .env path
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1i=%e)_bz$x_e#3+wwgfbx_g*htuwxubh5&8!!(%=^hxxe3+3z'
+SECRET_KEY = os.environ.get('SECRET_KEY')  
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -90,13 +94,25 @@ WSGI_APPLICATION = 'TeferetPROJECT.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+prod = os.environ.get('PROD_MODE')  
+if prod == "True":
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME':  os.environ.get('TEF_DB_NAME'),
+            'USER':  os.environ.get('TEF_USERNAME'),
+            'PASSWORD':  os.environ.get('TEF_PASSWORD'),
+            'HOST':  os.environ.get('TEF_HOSTNAME'),
+            'PORT':  os.environ.get('TEF_PORT'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
--- TeferetPROJECT/UserAuthsAPP/context_processors.py
+++ TeferetPROJECT/UserAuthsAPP/context_processors.py
@@ -5,6 +5,6 @@ def loggedUser(request):
     LoggedUserProfile = None
     if 'admin' in request.path:
         return dict(userProfile=LoggedUserProfile)
-    if request.user.is_authenticated:        
+    if request.user.is_authenticated and not request.user.is_superuser:        
         LoggedUserProfile = UserProfile.objects.get(user=request.user) 
     return dict(userProfile=LoggedUserProfile)
