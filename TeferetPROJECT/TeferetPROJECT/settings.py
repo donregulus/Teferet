98f0323a2f7358742547904139f285e735306f92
fossi <stroalgo@gmail.com>
2023-11-11 05:38:10 +0100

    domai,

--- TeferetPROJECT/TeferetPROJECT/settings.py
+++ TeferetPROJECT/TeferetPROJECT/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["teferet.pythonanywhere.com","localhost","127.0.0.1"]
+ALLOWED_HOSTS = ["teferet-production.up.railway.app","teferet.pythonanywhere.com","localhost","127.0.0.1"]
 
 
 # Application definition
