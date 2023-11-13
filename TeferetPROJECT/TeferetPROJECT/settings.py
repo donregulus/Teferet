4fff83904db160e14b23803a3c30e1825ea176f3
fossi <stroalgo@gmail.com>
2023-11-13 19:06:47 +0100

    français est mort

--- TeferetPROJECT/TeferetPROJECT/settings.py
+++ TeferetPROJECT/TeferetPROJECT/settings.py
@@ -61,7 +61,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
+    # "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -175,6 +175,7 @@ SESSION_ENGINE  = "django.contrib.sessions.backends.signed_cookies"
 STATIC_URL       = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 STATIC_ROOT      = os.path.join(BASE_DIR,'staticfiles')
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Media files (mp4, mp3, Images)
 MEDIA_URL        = '/media/'
